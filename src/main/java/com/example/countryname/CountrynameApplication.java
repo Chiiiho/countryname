d015a602b007724de4a6dfc5f2f1d673a5d9360c
chiiiho <163135461+Chiiiho@users.noreply.github.com>
2024-05-05 14:32:14 +0900

    プロジェクトを初期化

---
+++ src/main/java/com/example/countryname/CountrynameApplication.java
@@ -0,0 +1,13 @@
+package com.example.countryname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountrynameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountrynameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/countryname/CountrynameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countryname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountrynameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
